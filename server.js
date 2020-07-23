f07d05cfd1f077d9326a2ed42428bef800b6237e
abirami-murugesan <68633245+abirami-murugesan@users.noreply.github.com>
2020-07-23 20:36:43 +0530

    Update server.js
    xx

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 
 
-var connect = require('connect'),
-    serveStatic = require('serve-static');
+// var connect = require('connect'),
+//     serveStatic = require('serve-static');
 
-var app = connect();
+// var app = connect();
 
-app.use(serveStatic("../angularjs"));
+// app.use(serveStatic("../angularjs"));
 
 // app.use(connect.static(__dirname + '/index.html'));
 
 // app.use(serveStatic('../angularjs'),  {default: 'angular.min.js'}); app.listen(process.env.PORT || 3000); 
 // app.listen(5000);
+
+var express = require('express');  
+var app = express();  
+  
+app.use(express.static("app"));  
+  
+app.get('/', function (req, res) {  
+    res.redirect('/');  
+});  
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/');
+    console.log("----------------");
+});
+  
+app.listen(8080, 'localhost');  
+console.log("MyProject Server is Listening on port 8080");  
